32bd5afbd3e03c4865fc05bb89d45ae4b9c57863
jtachan <jaimeglez_005@hotmail.com>
2023-06-13 16:14:14 +0200

    New properties, errors and checks for matrix

---
+++ pymath_tools/instances/exceptions.py
@@ -0,0 +1,11 @@
+"""
+This module contains exceptions and errors to be raised among the instances.
+These errors are not to be added to __init__.py
+"""
+
+
+class DimensionError(ValueError):
+    """Invalid dimensions"""
+
+class SingularMatrixError(ValueError):
+    """Raised for invalid matrix properties in singular matrices"""
