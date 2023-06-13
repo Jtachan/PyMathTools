bfb659778857d9db033bb4e1e0f633d66f241355
jtachan <jaimeglez_005@hotmail.com>
2023-06-13 16:15:54 +0200

    Bugfix in unittest

--- pymath_tools/instances/exceptions.py
+++ pymath_tools/instances/exceptions.py
@@ -7,5 +7,6 @@ These errors are not to be added to __init__.py
 class DimensionError(ValueError):
     """Invalid dimensions"""
 
+
 class SingularMatrixError(ValueError):
     """Raised for invalid matrix properties in singular matrices"""
